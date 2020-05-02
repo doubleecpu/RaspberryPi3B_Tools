e89f4b22729d853d450ceba2f10352fe0b363724
Gilbert Medel <doubleecpu@outlook.com>
2020-05-01 21:16:45 -0700

    added sql execution

--- Sensor_Database/LAMP_Server/LAMP_Setup.sh
+++ Sensor_Database/LAMP_Server/LAMP_Setup.sh
@@ -1,38 +1,43 @@
 #!/bin bash
 echo "Starting Update -----------------------"
-apt-get update -y
+sudo apt-get update -y
 echo "Starting Upgrade -----------------------"
-apt-get upgrade -y
+sudo apt-get upgrade -y
 echo "autoremoving -----------------------"
-apt autoremove -y
+sudo apt autoremove -y
 echo "Installing Apache Webserver -----------------------"
 sudo apt-get install apache2 -y
 
 echo "Testing Apache on Raspberry Pi -----------------------"
 wget -O check_apache.html http://127.0.0.1 
-echo "Installing MariaDb -----------------------"
-sudo apt-get install mariadb-server -y
-sudo mysql_secure_installation
-sudo mysql -u root -p 
-use mysql; update user set plugin='' where User='root'; flush privileges; \q 
 echo "Installing PHP -----------------------"
-sudo apt install php php-mbstring php-mysql php-cli php-gd libapache2-mod-php php-cgi -y
+sudo apt install php php-mbstring php-cli php-gd libapache2-mod-php php-cgi -y
+
+echo "Installing MariaDb -----------------------"
+echo "Please enter root user MySQL password!"
+read rootpasswd
+sudo apt-get install mariadb-server php-mysql -y
+sudo service apache2 restart
+sudo mysql_secure_installation
 
 echo "Changing ownership to pi user -----------------------"
 cd /var/www/html
 sudo chown -R pi:www-data /var/www/html/*
-sudo chmod -R 770 /var/www/html/*
+sudo chmod -R 777 /var/www/html/*
 sudo rm index.html
 sudo touch index.php
 echo "Please Add Test Message To index.php -----------------------"
 sudo echo "<?php phpinfo(); ?>" >> /var/www/html/index.php
-sudo nano index.php
-
+#sudo nano index.php
+wget -O check_php.html http://127.0.0.1 
 echo "Restaring apache service -----------------------"
 sudo service apache2 restart
 echo "Installing PHP Admin Please Configure Database -------------" 
-sudo apt install phpmyadmin -y
-
+sudo apt-get install phpmyadmin -y
+sudo mysql -u root -p${rootpasswd} -e "use mysql; update user set plugin='' where User='root'; flush privileges; \q"
+sudo mysql -u root -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON mysql.* TO 'root'@'localhost'; FLUSH PRIVILEGES; SHOW DATABASES;"
+echo "Database and User Created!"
+echo "adjusting PHP configuration  -------------"
 cd /etc/apache2/conf-available
 sudo ln -s ../../phpmyadmin/apache.conf phpmyadmin.conf
 cd /etc/apache2/conf-enabled
