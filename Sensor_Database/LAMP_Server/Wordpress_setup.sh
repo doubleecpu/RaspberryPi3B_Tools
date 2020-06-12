64a889bd36a396b9b8a47aed8de4a79b0f6d558d
Gilbert Medel <doubleecpu@outlook.com>
2020-06-12 13:29:16 -0700

    Added Saved Password as script runs

--- Sensor_Database/LAMP_Server/Wordpress_setup.sh
+++ Sensor_Database/LAMP_Server/Wordpress_setup.sh
@@ -6,6 +6,9 @@ sudo wget http://wordpress.org/latest.tar.gz
 sudo tar xzf latest.tar.gz 
 sudo mv wordpress/* .
 tree -L 1 
+echo -n "Please enter root user MySQL password!"
+read -s rootpasswd
+echo
 sudo chown -R www-data: .
-sudo mysql -u root -p -e "create database wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY '"${rootpasswd}"'; FLUSH PRIVILEGES; \q"
+sudo mysql -u root -p${rootpasswd} -e "create database wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY '"${rootpasswd}"'; FLUSH PRIVILEGES; \q"
 echo "The WordPress website should now be running. open web browser and enter http://localhost this should open the setup-config.php webpage."
