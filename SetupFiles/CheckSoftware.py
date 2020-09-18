c666695218bbd447b5fdd0759ede8508bd4919a8
Gilbert Medel <doubleecpu@outlook.com>
2020-09-17 19:03:10 -0700

    Testing File cloning to Raspberry Pi

---
+++ SetupFiles/CheckSoftware.py
@@ -0,0 +1,30 @@
+
+import platform; 
+
+print(platform.linux_distribution())'
+
+
+
+print("Checking Version of Linux")
+
+print( "Checking Version Python")
+
+print( "Checking Version GPIO")
+
+print( "Checking Version I2C")
+
+print( "Checking Version SPI")
+
+print( "Checking Version UART")
+
+print( "Checking MySQL")
+
+print( "Checking Apache")
+
+print( "Checking PHP")
+
+print( "Checking NodeJS")
+
+print( "Checking Wordpress")
+
+print( "Finished Checking Software")
